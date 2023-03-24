440957d71d4dec64b1e4580d27642943497a632c
Van Hien <hiench2001@gmail.com>
2023-03-24 15:45:34 -0600

    Main test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,22 @@
-import { NextApiResponse } from 'next';
+import AWS from 'aws-sdk';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler( res:NextApiResponse) {
-  res.status(200).json({ name: 'John Doe' })
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'ca-central-1',
+});
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const data = await s3.getObject({
+      Bucket: 'cakestore',
+      Key: 'path/to/object'
+    }).promise();
+    
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 }
