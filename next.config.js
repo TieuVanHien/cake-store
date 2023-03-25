e6f185c22330808110ccf3f80bb358207839b91b
Van Hien <hiench2001@gmail.com>
2023-03-24 18:42:14 -0600

    updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = nextConfig;
-
-module.exports = {
   experimental: {
     forceSwcTransforms: true,
   },
 };
+
+module.exports = nextConfig;
